7b66e072cb4ff9cf76793ec9bbde37d020efd076
bandianhong <scottban@126.com>
2019-03-26 14:58:32 +0800

    a

---
+++ FDBankCardModel.h
@@ -0,0 +1,340 @@
+//
+//Created by ESJsonFormatForMac on 18/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FDBankCardModel : NSObject
+
+@property (nonatomic, copy) NSString *CDCB;
+
+@property (nonatomic, copy) NSString *BHB;
+
+@property (nonatomic, copy) NSString *LSBC;
+
+@property (nonatomic, copy) NSString *JXRCU;
+
+@property (nonatomic, copy) NSString *KSRB;
+
+@property (nonatomic, copy) NSString *SXCB;
+
+@property (nonatomic, copy) NSString *BOCY;
+
+@property (nonatomic, copy) NSString *COMM;
+
+@property (nonatomic, copy) NSString *GZB;
+
+@property (nonatomic, copy) NSString *LSCCB;
+
+@property (nonatomic, copy) NSString *GLBANK;
+
+@property (nonatomic, copy) NSString *JJBANK;
+
+@property (nonatomic, copy) NSString *TCRCB;
+
+@property (nonatomic, copy) NSString *DZBANK;
+
+@property (nonatomic, copy) NSString *JSBANK;
+
+@property (nonatomic, copy) NSString *MTBANK;
+
+@property (nonatomic, copy) NSString *SJBANK;
+
+@property (nonatomic, copy) NSString *CBKF;
+
+@property (nonatomic, copy) NSString *QDCCB;
+
+@property (nonatomic, copy) NSString *BOCD;
+
+@property (nonatomic, copy) NSString *HNRCU;
+
+@property (nonatomic, copy) NSString *FDB;
+
+@property (nonatomic, copy) NSString *CIB;
+
+@property (nonatomic, copy) NSString *ZJKCCB;
+
+@property (nonatomic, copy) NSString *GYCB;
+
+@property (nonatomic, copy) NSString *YNRCC;
+
+@property (nonatomic, copy) NSString *GCB;
+
+@property (nonatomic, copy) NSString *CQBANK;
+
+@property (nonatomic, copy) NSString *SPABANK;
+
+@property (nonatomic, copy) NSString *CZBANK;
+
+@property (nonatomic, copy) NSString *WJRCB;
+
+@property (nonatomic, copy) NSString *ORBANK;
+
+@property (nonatomic, copy) NSString *GDB;
+
+@property (nonatomic, copy) NSString *XLBANK;
+
+@property (nonatomic, copy) NSString *HBC;
+
+@property (nonatomic, copy) NSString *NJCB;
+
+@property (nonatomic, copy) NSString *CSCB;
+
+@property (nonatomic, copy) NSString *DYCCB;
+
+@property (nonatomic, copy) NSString *XXBANK;
+
+@property (nonatomic, copy) NSString *ZJNX;
+
+@property (nonatomic, copy) NSString *TCCB;
+
+@property (nonatomic, copy) NSString *WZCB;
+
+@property (nonatomic, copy) NSString *SZSBK;
+
+@property (nonatomic, copy) NSString *SDEB;
+
+@property (nonatomic, copy) NSString *ASCB;
+
+@property (nonatomic, copy) NSString *HZCB;
+
+@property (nonatomic, copy) NSString *KORLABANK;
+
+@property (nonatomic, copy) NSString *HDBANK;
+
+@property (nonatomic, copy) NSString *CMB;
+
+@property (nonatomic, copy) NSString *ZGCCB;
+
+@property (nonatomic, copy) NSString *HRXJB;
+
+@property (nonatomic, copy) NSString *HSBANK;
+
+@property (nonatomic, copy) NSString *NHQS;
+
+@property (nonatomic, copy) NSString *NXBANK;
+
+@property (nonatomic, copy) NSString *DLB;
+
+@property (nonatomic, copy) NSString *BOC;
+
+@property (nonatomic, copy) NSString *CRCBANK;
+
+@property (nonatomic, copy) NSString *XCYH;
+
+@property (nonatomic, copy) NSString *YBCCB;
+
+@property (nonatomic, copy) NSString *CITIC;
+
+@property (nonatomic, copy) NSString *BOD;
+
+@property (nonatomic, copy) NSString *AYCB;
+
+@property (nonatomic, copy) NSString *CDRCB;
+
+@property (nonatomic, copy) NSString *ZBCB;
+
+@property (nonatomic, copy) NSString *LZYH;
+
+@property (nonatomic, copy) NSString *HURCB;
+
+@property (nonatomic, copy) NSString *GRCB;
+
+@property (nonatomic, copy) NSString *BOYK;
+
+@property (nonatomic, copy) NSString *GDRCC;
+
+@property (nonatomic, copy) NSString *JHBANK;
+
+@property (nonatomic, copy) NSString *JNBANK;
+
+@property (nonatomic, copy) NSString *SHBANK;
+
+@property (nonatomic, copy) NSString *JZBANK;
+
+@property (nonatomic, copy) NSString *TRCB;
+
+@property (nonatomic, copy) NSString *JINCHB;
+
+@property (nonatomic, copy) NSString *ARCU;
+
+@property (nonatomic, copy) NSString *BANKWF;
+
+@property (nonatomic, copy) NSString *BSB;
+
+@property (nonatomic, copy) NSString *LYCB;
+
+@property (nonatomic, copy) NSString *GZRCU;
+
+@property (nonatomic, copy) NSString *WHRCB;
+
+@property (nonatomic, copy) NSString *CCQTGB;
+
+@property (nonatomic, copy) NSString *HSBK;
+
+@property (nonatomic, copy) NSString *BODD;
+
+@property (nonatomic, copy) NSString *ZRCBANK;
+
+@property (nonatomic, copy) NSString *XABANK;
+
+@property (nonatomic, copy) NSString *HKB;
+
+@property (nonatomic, copy) NSString *CGNB;
+
+@property (nonatomic, copy) NSString *XYBANK;
+
+@property (nonatomic, copy) NSString *SHRCB;
+
+@property (nonatomic, copy) NSString *HNRCC;
+
+@property (nonatomic, copy) NSString *GSRCU;
+
+@property (nonatomic, copy) NSString *FSCB;
+
+@property (nonatomic, copy) NSString *BOSZ;
+
+@property (nonatomic, copy) NSString *YDRCB;
+
+@property (nonatomic, copy) NSString *BOHAIB;
+
+@property (nonatomic, copy) NSString *EGBANK;
+
+@property (nonatomic, copy) NSString *NCB;
+
+@property (nonatomic, copy) NSString *BOP;
+
+@property (nonatomic, copy) NSString *ZYCBANK;
+
+@property (nonatomic, copy) NSString *FJHXBC;
+
+@property (nonatomic, copy) NSString *CBBQS;
+
+@property (nonatomic, copy) NSString *BZMD;
+
+@property (nonatomic, copy) NSString *HKBEA;
+
+@property (nonatomic, copy) NSString *ZZBANK;
+
+@property (nonatomic, copy) NSString *BJRCB;
+
+@property (nonatomic, copy) NSString *HBHSBANK;
+
+@property (nonatomic, copy) NSString *HANABANK;
+
+@property (nonatomic, copy) NSString *JLBANK;
+
+@property (nonatomic, copy) NSString *DYCB;
+
+@property (nonatomic, copy) NSString *HBYCBANK;
+
+@property (nonatomic, copy) NSString *JXBANK;
+
+@property (nonatomic, copy) NSString *ABC;
+
+@property (nonatomic, copy) NSString *LANGFB;
+
+@property (nonatomic, copy) NSString *DRCBCL;
+
+@property (nonatomic, copy) NSString *SRBANK;
+
+@property (nonatomic, copy) NSString *KLB;
+
+@property (nonatomic, copy) NSString *CMBC;
+
+@property (nonatomic, copy) NSString *ICBC;
+
+@property (nonatomic, copy) NSString *SCCB;
+
+@property (nonatomic, copy) NSString *ZJTLCB;
+
+@property (nonatomic, copy) NSString *JRCB;
+
+@property (nonatomic, copy) NSString *BOZK;
+
+@property (nonatomic, copy) NSString *TACCB;
+
+@property (nonatomic, copy) NSString *NBYZ;
+
+@property (nonatomic, copy) NSString *H3CB;
+
+@property (nonatomic, copy) NSString *NHB;
+
+@property (nonatomic, copy) NSString *JSRCU;
+
+@property (nonatomic, copy) NSString *GXRCU;
+
+@property (nonatomic, copy) NSString *SDRCU;
+
+@property (nonatomic, copy) NSString *WRCB;
+
+@property (nonatomic, copy) NSString *LSBANK;
+
+@property (nonatomic, copy) NSString *TZCB;
+
+@property (nonatomic, copy) NSString *LYBANK;
+
+@property (nonatomic, copy) NSString *URMQCCB;
+
+@property (nonatomic, copy) NSString *NYNB;
+
+@property (nonatomic, copy) NSString *PSBC;
+
+@property (nonatomic, copy) NSString *YXCCB;
+
+@property (nonatomic, copy) NSString *CCB;
+
+@property (nonatomic, copy) NSString *CZRCB;
+
+@property (nonatomic, copy) NSString *JLRCU;
+
+@property (nonatomic, copy) NSString *WHCCB;
+
+@property (nonatomic, copy) NSString *XTB;
+
+@property (nonatomic, copy) NSString *CDB;
+
+@property (nonatomic, copy) NSString *DAQINGB;
+
+@property (nonatomic, copy) NSString *BOJZ;
+
+@property (nonatomic, copy) NSString *BJBANK;
+
+@property (nonatomic, copy) NSString *NBBANK;
+
+@property (nonatomic, copy) NSString *CZCB;
+
+@property (nonatomic, copy) NSString *SRCB;
+
+@property (nonatomic, copy) NSString *HXBANK;
+
+@property (nonatomic, copy) NSString *SCRCU;
+
+@property (nonatomic, copy) NSString *CEB;
+
+@property (nonatomic, copy) NSString *QLBANK;
+
+@property (nonatomic, copy) NSString *JSB;
+
+@property (nonatomic, copy) NSString *YQCCB;
+
+@property (nonatomic, copy) NSString *BGB;
+
+@property (nonatomic, copy) NSString *NXRCU;
+
+@property (nonatomic, copy) NSString *FXCB;
+
+@property (nonatomic, copy) NSString *BOQH;
+
+@property (nonatomic, copy) NSString *HBRCU;
+
+@property (nonatomic, copy) NSString *CSRCB;
+
+@property (nonatomic, copy) NSString *SXRCCU;
+
+@property (nonatomic, copy) NSString *SPDB;
+
+@property (nonatomic, copy) NSString *HZCCB;
+
+@end
---
+++ FDBankCardModel.m
@@ -0,0 +1,10 @@
+//
+//Created by ESJsonFormatForMac on 18/12/21.
+//
+
+#import "FDBankCardModel.h"
+@implementation FDBankCardModel
+
+
+@end
+
